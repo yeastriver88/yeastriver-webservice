ee2282420c29f66f6f6e0875f6586856d8e518fe
yeastriver88 <yeastriver88@gmail.com>
2022-10-22 22:21:39 +0900

    commit test

---
+++ src/main/java/com/yeastriver/webservice/yeastriverwebservice/YeastriverWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.yeastriver.webservice.yeastriverwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeastriverWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeastriverWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeastriver/webservice/yeastriverwebservice/YeastriverWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeastriver.webservice.yeastriverwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeastriverWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
